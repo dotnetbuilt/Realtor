cf4fb4ca966c5d4776c65ba1a7f7bcf86515c9e6
abbosZoirov1 <122oszoirov@gmail.com>
2023-10-10 18:50:23 +0500

    Add Auditable

---
+++ src/Realtor.Domain/Commons/Auditable.cs
@@ -0,0 +1,9 @@
+namespace Realtor.Domain.Commons;
+
+public abstract class Auditable
+{
+    public Guid Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public bool IsDeleted { get; set; } = false;
+}
