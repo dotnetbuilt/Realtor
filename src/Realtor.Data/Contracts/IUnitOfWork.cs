681e3332f45f0021b52bfba5b8dba2f3144c55e6
abbosZoirov1 <122oszoirov@gmail.com>
2023-10-10 19:22:18 +0500

    Add IUnitOfWork

---
+++ src/Realtor.Data/Contracts/IUnitOfWork.cs
@@ -0,0 +1,25 @@
+using Realtor.Domain.Entities;
+
+namespace Realtor.Data.Contracts;
+
+public interface IUnitOfWork:IDisposable
+{
+    IRepository<User> UserRepository { get; }
+    IRepository<UserProfile> UserProfileRepository { get; }
+    IRepository<Attachment> AttachmentRepository { get; }
+    IRepository<Address> AddressRepository { get; }
+    IRepository<Country> CountryRepository { get; }
+    IRepository<Region> RegionRepository { get; }
+    IRepository<District> DistrictRepository { get; }
+    IRepository<Neighborhood> NeighborhoodRepository { get; }
+    IRepository<Apartment> ApartmentRepository { get; }
+    IRepository<ApartmentBlock> ApartmentBlockRepository { get; }
+    IRepository<ApartmentBlockPart> ApartmentBlockPartRepository { get; }
+    IRepository<Cottage> CottageRepository { get; }
+    IRepository<CottageBlock> CottageBlockRepository { get; }
+    IRepository<CottageBlockPart> CottageBlockPartRepository { get; }
+    IRepository<Property> PropertyRepository { get; }
+    IRepository<Link> LinkRepository { get; }
+    IRepository<Phone> PhoneRepository { get; }
+    ValueTask SaveAsync();
+}
