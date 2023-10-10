d472dd6742fee27537b886a1b6c2b0a864a7d534
abbosZoirov1 <122oszoirov@gmail.com>
2023-10-10 20:04:19 +0500

    Add Apartments

---
+++ src/Realtor.Service/DTOs/Apartments/ApartmentResultDto.cs
@@ -0,0 +1,21 @@
+using Realtor.Domain.Entities;
+using Realtor.Domain.Enums;
+using Realtor.Service.DTOs.Addresses;
+using Realtor.Service.DTOs.Properties;
+using Realtor.Service.DTOs.Users;
+
+namespace Realtor.Service.DTOs.Apartments;
+
+public class ApartmentResultDto
+{
+    public long Id { get; set; }
+    public PropertyResultDto PropertyResultDto { get; set; }
+    public int NumberOfFloors { get; set; }
+    public int Floor { get; set; }
+    public int NumberOfRooms { get; set; }
+    public int UsageArea { get; set; }
+    public AreaUnit UsageAreaUnit { get; set; }
+    public ICollection<Attachment> Attachments { get; set; }
+    public UserResultDto UserProfileResultDto { get; set; }
+    public AddressResultDto AddressResultDto { get; set; }
+}
---
+++ src/Realtor.Service/DTOs/Apartments/ApartmentUpdateDto.cs
@@ -0,0 +1,18 @@
+using Realtor.Domain.Entities;
+using Realtor.Domain.Enums;
+
+namespace Realtor.Service.DTOs.Apartments;
+
+public class ApartmentUpdateDto
+{
+    public long Id { get; set; }
+    public long PropertyId { get; set; }
+    public int NumberOfFloors { get; set; }
+    public int Floor { get; set; }
+    public int NumberOfRooms { get; set; }
+    public int UsageArea { get; set; }
+    public AreaUnit UsageAreaUnit { get; set; }
+    public ICollection<Attachment> Attachments { get; set; }
+    public long UserId { get; set; }
+    public long AddressId { get; set; }
+}
