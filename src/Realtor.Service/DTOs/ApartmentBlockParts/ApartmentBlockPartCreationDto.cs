2158543ad841f98271bfd354f5658d423f9ac12e
abbosZoirov1 <122oszoirov@gmail.com>
2023-10-10 20:06:39 +0500

    Add ApartmentBlockParts

---
+++ src/Realtor.Service/DTOs/ApartmentBlockParts/ApartmentBlockPartCreationDto.cs
@@ -0,0 +1,12 @@
+using Realtor.Domain.Enums;
+
+namespace Realtor.Service.DTOs.ApartmentBlockParts;
+
+public class ApartmentBlockPartCreationDto
+{
+    public int NumberOfRooms { get; set; }
+    public int UsageArea { get; set; }
+    public AreaUnit UsageAreaUnit { get; set; }
+    public decimal Cost { get; set; }
+    public Currency Currency { get; set; }
+}
---
+++ src/Realtor.Service/DTOs/ApartmentBlockParts/ApartmentBlockPartUpdateDto.cs
@@ -0,0 +1,13 @@
+using Realtor.Domain.Enums;
+
+namespace Realtor.Service.DTOs.ApartmentBlockParts;
+
+public class ApartmentBlockPartUpdateDto
+{
+    public long Id { get; set; }
+    public int NumberOfRooms { get; set; }
+    public int UsageArea { get; set; }
+    public AreaUnit UsageAreaUnit { get; set; }
+    public decimal Cost { get; set; }
+    public Currency Currency { get; set; }
+}
