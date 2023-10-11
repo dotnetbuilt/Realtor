7a149832de682b67520cbf6c17e6332296e57919
abbosZoirov1 <122oszoirov@gmail.com>
2023-10-11 10:34:31 +0500

    Add Extensions

---
+++ src/Realtor.Service/Extensions/CollectionPagination.cs
@@ -0,0 +1,9 @@
+using Realtor.Domain.Configurations;
+
+namespace Realtor.Service.Extensions;
+
+public static class CollectionPagination
+{
+    public static IQueryable<T> ToPaginate<T>(this IQueryable<T> values, PaginationParams @params)
+        => values.Skip((@params.PageIndex - 1) * @params.PageSize).Take(@params.PageSize);
+}
---
+++ src/Realtor.Service/Extensions/PasswordHasher.cs
@@ -0,0 +1,10 @@
+namespace Realtor.Service.Extensions;
+
+public static class PasswordHasher
+{
+    public static string Hash(this string password) 
+        => BCrypt.Net.BCrypt.HashPassword(password);
+
+    public static bool Verify(this string password, string hashedPassword) 
+        => BCrypt.Net.BCrypt.Verify(password, hashedPassword);
+}
