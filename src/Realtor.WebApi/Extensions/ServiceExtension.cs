51e5e64fb7e91b8f830b06797f579a7ddc8ec3ad
abbosZoirov1 <122oszoirov@gmail.com>
2023-10-11 12:15:09 +0500

    Add ServiceExtension

--- src/Realtor.WebApi/Extensions/ServiceExtension.cs
+++ src/Realtor.WebApi/Extensions/ServiceExtension.cs
@@ -1,5 +1,7 @@
+using System.Runtime.Intrinsics.X86;
 using Realtor.Data.Contracts;
 using Realtor.Data.Repositories;
+using Realtor.Domain.Entities;
 using Realtor.Service.Interfaces;
 using Realtor.Service.Services;
 
@@ -15,5 +17,18 @@ public static class ServiceExtension
         services.AddScoped<IAuthService, AuthService>();
         services.AddScoped<IUserProfileService, UserProfileService>();
         services.AddScoped<IAddressService, AddressService>();
+        services.AddScoped<ICountryService, CountryService>();
+        services.AddScoped<IRegionService, RegionService>();
+        services.AddScoped<IDistrictService, DistrictService>();
+        services.AddScoped<INeighborhoodService, NeighborhoodService>();
+        services.AddScoped<IPropertyService, PropertyService>();
+        services.AddScoped<ILinkService, LinkService>();
+        services.AddScoped<IPhoneService, PhoneService>();
+        services.AddScoped<IApartmentService, ApartmentService>();
+        services.AddScoped<IApartmentBlockService, ApartmentBlockService>();
+        services.AddScoped<IApartmentBlockPartService, ApartmentBlockPartService>();
+        services.AddScoped<ICottageService, CottageService>();
+        services.AddScoped<ICottageBlockService, CottageBlockService>();
+        services.AddScoped<ICottageBlockPartService, CottageBlockPartService>();
     }
 }
