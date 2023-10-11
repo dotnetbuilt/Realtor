914f3a0b4b47e0541d8c98f9bb39753c0554ab36
abbosZoirov1 <122oszoirov@gmail.com>
2023-10-11 13:21:31 +0500

    Add ApartmentBlockPartsController

---
+++ src/Realtor.WebApi/Controllers/ApartmentBlockPartsController.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Mvc;
+using Realtor.Service.DTOs.ApartmentBlockParts;
+using Realtor.Service.Interfaces;
+using Realtor.WebApi.Models;
+
+namespace Realtor.WebApi.Controllers;
+
+public class ApartmentBlockPartsController : BaseController
+{
+    private readonly IApartmentBlockPartService _service;
+
+    public ApartmentBlockPartsController(IApartmentBlockPartService service)
+    {
+        _service = service;
+    }
+
+    [HttpPost("create")]
+    public async ValueTask<IActionResult> CreateAsync(ApartmentBlockPartCreationDto dto)
+        => Ok(new Responce()
+        {
+            StatusCode = 200,
+            Message = "Ok",
+            Data = await _service.AddAsync(dto)
+        });
+
+    [HttpPut("update")]
+    public async ValueTask<IActionResult> UpdateAsync(ApartmentBlockPartUpdateDto dto)
+        => Ok(new Responce()
+        {
+            StatusCode = 200,
+            Message = "Ok",
+            Data = await _service.ModifyAsync(dto)
+        });
+
+    [HttpDelete("delete")]
+    public async ValueTask<IActionResult> DeleteAsync(long id)
+        => Ok(new Responce()
+        {
+            StatusCode = 200,
+            Message = "Ok",
+            Data = await _service.RetrieveByIdAsync(id)
+        });
+
+    [HttpGet("get-by-id")]
+    public async ValueTask<IActionResult> GetByIdAsync(long id)
+        => Ok(new Responce()
+        {
+            StatusCode = 200,
+            Message = "Ok",
+            Data = await _service.RetrieveByIdAsync(id)
+        });
+}
