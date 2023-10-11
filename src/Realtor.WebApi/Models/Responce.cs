6e44fe30e4db8a352990125fecdc305e9992be17
abbosZoirov1 <122oszoirov@gmail.com>
2023-10-11 12:17:31 +0500

    Add Responce

---
+++ src/Realtor.WebApi/Models/Responce.cs
@@ -0,0 +1,8 @@
+namespace Realtor.WebApi.Models;
+
+public class Responce
+{
+    public int StatusCode { get; set; }
+    public string Message { get; set; }
+    public object Data { get; set; }
+}
