4239850b605dbd69a2684a12bc0a7d847dbfc79f
abbosZoirov1 <122oszoirov@gmail.com>
2023-10-11 10:30:43 +0500

    Add Exceptions

---
+++ src/Realtor.Service/Exceptions/AlreadyExistsException.cs
@@ -0,0 +1,12 @@
+namespace Realtor.Service.Exceptions;
+
+public class AlreadyExistsException:Exception
+{
+    public int StatusCode { get; set; } = 403;
+
+    public AlreadyExistsException(string message):base(message)
+    { }
+
+    public AlreadyExistsException(string message,Exception innerException):base(message,innerException)
+    { }
+}
---
+++ src/Realtor.Service/Exceptions/CustomException.cs
@@ -0,0 +1,11 @@
+namespace Realtor.Service.Exceptions;
+
+public class CustomException:Exception
+{
+    public int StatusCode { get; set; }
+    
+    public CustomException(string message,int statuscode):base(message)
+    {
+        StatusCode = statuscode;
+    }
+}
