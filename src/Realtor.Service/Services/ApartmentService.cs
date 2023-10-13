44160282af6735f4ec14832471a7b7794f2c4b77
abbosZoirov1 <122oszoirov@gmail.com>
2023-10-13 16:38:28 +0500

    Fix Bugs

--- src/Realtor.Service/Services/ApartmentService.cs
+++ src/Realtor.Service/Services/ApartmentService.cs
@@ -21,7 +21,25 @@ public class ApartmentService:IApartmentService
 
     public async ValueTask<ApartmentResultDto> AddAsync(ApartmentCreationDto dto)
     {
+        var existUser = await _unitOfWork.UserRepository.SelectAsync(expression: user => user.Id == dto.UserId)
+                        ?? throw new NotFoundException(message: "User is not found");
+
+        var existProperty =
+            await _unitOfWork.PropertyRepository.SelectAsync(expression: property => property.Id == dto.PropertyId)
+            ?? throw new NotFoundException(message: "Property is not found");
+
+        var existAddress = await _unitOfWork.AddressRepository
+            .SelectAsync(expression: address => address.Id == dto.AddressId,includes:
+                new[]{"Neighborhood.District.Region.Country","District.Region.Country","Region.Country","Country"})
+            ?? throw new NotFoundException(message: "Address is not found");
+        
         var mappedApartment = _mapper.Map<Apartment>(source: dto);
+        mappedApartment.AddressId = existAddress.Id;
+        mappedApartment.Address = existAddress;
+        mappedApartment.UserId = existUser.Id;
+        mappedApartment.User = existUser;
+        mappedApartment.PropertyId = existProperty.Id;
+        mappedApartment.Property = existProperty;
 
         await _unitOfWork.ApartmentRepository.CreateAsync(entity: mappedApartment);
         await _unitOfWork.SaveAsync();
@@ -32,7 +50,8 @@ public class ApartmentService:IApartmentService
     public async ValueTask<ApartmentResultDto> ModifyAsync(ApartmentUpdateDto dto)
     {
         var existApartment = await _unitOfWork.ApartmentRepository
-            .SelectAsync(apartment => apartment.Id == dto.Id);
+            .SelectAsync(expression:apartment => apartment.Id == dto.Id,
+                includes:new[]{"User","Address","Property"});
 
         if (existApartment == null)
             throw new NotFoundException(message: "Apartment is not found");
@@ -48,7 +67,7 @@ public class ApartmentService:IApartmentService
     public async ValueTask<bool> RemoveAsync(long id)
     {
         var existApartment = await _unitOfWork.ApartmentRepository
-            .SelectAsync(apartment => apartment.Id == id);
+            .SelectAsync(expression:apartment => apartment.Id == id);
 
         if (existApartment == null)
             throw new NotFoundException(message: "Apartment is not found");
@@ -62,7 +81,8 @@ public class ApartmentService:IApartmentService
     public async ValueTask<ApartmentResultDto> RetrieveByIdAsync(long id)
     {
         var existApartment = await _unitOfWork.ApartmentRepository
-            .SelectAsync(expression:apartment => apartment.Id == id);
+            .SelectAsync(expression:apartment => apartment.Id == id,
+                includes:new[]{"User","Address","Property"});
 
         if (existApartment == null)
             throw new NotFoundException(message: "Apartment is not found");
@@ -73,7 +93,8 @@ public class ApartmentService:IApartmentService
     public async ValueTask<IEnumerable<ApartmentResultDto>> RetrieveAllByUserIdAsync(long userId)
     {
         var apartments = await _unitOfWork.ApartmentRepository
-            .SelectAll(expression:apartment => apartment.UserId == userId)
+            .SelectAll(expression:apartment => apartment.UserId == userId,
+                includes:new[]{"User","Address","Property"})
             .ToListAsync();
 
         return _mapper.Map<IEnumerable<ApartmentResultDto>>(source: apartments);
